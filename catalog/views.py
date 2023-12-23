8d2634c9a905e0f225678cff5bb434357d2a6fd5
alexander <vertiporogov.alexandr@mail.ru>
2023-12-23 20:57:12 +0300

    finish

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 
+from catalog.models import Category, Product
+
 
 def home(request):
-    return render(request, 'home.html')
+
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Магазин всего - Главная'
+    }
+
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
+    context = {
+        'title': 'Контакты'
+    }
+
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
@@ -13,7 +25,13 @@ def contacts(request):
 
         print(f'{name} ({phone}): {message}')
 
-    return render(request, 'contact.html')
+    return render(request, 'catalog/contact.html', context)
+
 
 def products(request):
-    return render(request, 'products.html')
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Товар'
+    }
+
+    return render(request, 'catalog/products.html', context)
