7607ab44550db5f7960444077e6561868df1c2a6
alexander <vertiporogov.alexandr@mail.ru>
2024-01-03 22:43:54 +0300

    na segodni hvatit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
+from blog.apps import BlogConfig
+from blog.views import BlogCreateView, HomeView
 
+app_name = BlogConfig.name
 
 urlpatterns = [
-
+    path('create/', BlogCreateView.as_view, name='create_blog'),
+    path('home/', HomeView.as_view(), name='home2'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
+from pytils.translit import slugify
 
-# Create your views here.
+from blog.models import Blog
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'description', 'photo',)
+    success_url = reverse_lazy('blog:home')
+
+    # def form_valid(self, form):
+    #     if form.is_valid:
+    #         new_blog = form.save()
+    #         new_blog.slug = slugify(new_blog.title)
+    #         new_blog.save()
+    #
+    #     return super().form_valid(form)
+
+
+class HomeView(TemplateView):
+    template_name = 'catalog/home.html'
+    extra_context = {
+        'title': 'Магазин всего - Главная'
+    }
