8e2552c2828447d199e372fb04a572d437cc60b6
alexander <vertiporogov.alexandr@mail.ru>
2024-01-04 12:12:43 +0300

    fixed second

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogCreateView, HomeView
+from blog.views import BlogCreateView, HomeView, BlogUpdateView, BlogListView, BlogDetailView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('create/', BlogCreateView.as_view, name='create_blog'),
     path('home/', HomeView.as_view(), name='home2'),
+    path('create/', BlogCreateView.as_view(), name='create_blog'),
+    path('update/<int:pk>', BlogUpdateView.as_view(), name='update_blog'),
+    path('view/<int:pk>', BlogDetailView.as_view(), name='detail_blog'),
+    path('list/', BlogListView.as_view(), name='list_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView, ListView, DetailView
 from pytils.translit import slugify
 
 from blog.models import Blog
@@ -9,19 +9,41 @@ from blog.models import Blog
 class BlogCreateView(CreateView):
     model = Blog
     fields = ('title', 'description', 'photo',)
-    success_url = reverse_lazy('blog:home')
+    success_url = reverse_lazy('blog:home2')
 
-    # def form_valid(self, form):
-    #     if form.is_valid:
-    #         new_blog = form.save()
-    #         new_blog.slug = slugify(new_blog.title)
-    #         new_blog.save()
-    #
-    #     return super().form_valid(form)
+    def form_valid(self, form):
+        if form.is_valid:
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+
+        return super().form_valid(form)
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'description', 'photo',)
+    success_url = reverse_lazy('blog:home2')
+
+    def form_valid(self, form):
+        if form.is_valid:
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+
+        return super().form_valid(form)
+
+
+class BlogListView(ListView):
+    model = Blog
+
+
+class BlogDetailView(DetailView):
+    model = Blog
 
 
 class HomeView(TemplateView):
-    template_name = 'catalog/home.html'
+    template_name = 'blog/home.html'
     extra_context = {
-        'title': 'Магазин всего - Главная'
+        'title': 'Блогерная'
     }
